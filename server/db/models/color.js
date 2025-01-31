64387dff6bb0b3dd108bc4f044dabef1ed3cacf0
demirose41 <demirose41@gmail.com>
2025-01-30 20:02:30 -0800

    completed practice

--- server/db/models/color.js
+++ server/db/models/color.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    
     static associate(models) {
       // define association here
     }
@@ -17,6 +18,19 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        // must be between 2 and 20 characters long
+        checkLength(value) {
+          if(value.length < 2 || value.length > 20){
+            throw new Error("Name must be between 2 and 20 characters long")
+          }
+        },
+        endsIn(value) {
+          if(value.slice(-1) === 'y'){
+            throw new Error("Color can not end with 'y'")
+          }
+        } 
+      }
     }
   }, {
     sequelize,
